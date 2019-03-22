f9204622cd5f219ce12aa6fa584089a07ed534d7
EynerEdins <eyner.davila@gmail.com>
2019-03-22 00:22:24 -0500

    inicio proyecto

---
+++ src/main/java/com/web/backend/ContaWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaWebBackendApplication.class, args);
+	}
+
+}
